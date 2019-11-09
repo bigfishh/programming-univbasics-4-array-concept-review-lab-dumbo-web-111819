7eba0973688ece2bbe1e1c9aa7f4514c03174f2c
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-09 03:56:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,6 +20,8 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  array.sort!
-  array.first
+  min = 0 
+  length = array.length 
+  length.times do |index|
+    if array[index] < min 
 end
